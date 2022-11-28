763e90de5704aa6eb3167247764e94352f4d5cd5
arturikinik <artur-kondratev-1998@mail.ru>
2022-11-28 15:22:53 +0700

    добавление второй функции

--- interestingCodeFromFreeCodeCamp.js
+++ interestingCodeFromFreeCodeCamp.js
@@ -3,3 +3,8 @@ function functionWithArgs(param1, param2) {
   }
   functionWithArgs(1,3);
   // Функиця суммирующая два своих аргумента
+  function timesFive(num) {
+    return num*5;
+  }
+// Функция умножающая аргумент на 5
+  
